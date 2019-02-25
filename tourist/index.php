9166d08141c7d1fdf60f3720ab5cd3bc1772a08a
winnie <winnie131212592@gmail.com>
2019-02-26 04:02:18 +0800

    add movie title below phot

--- tourist/index.php
+++ tourist/index.php
@@ -19,12 +19,16 @@
 			<?php 
 				$b_id = $r['b_id'];
 		  		$bImage = $r['b_img'];
+		  		$title = $r['moviename'];
 			 ?>
 			 <center>
 			 	<div class="col-lg-3 col-md-4 col-sm-6">
 					<a href="#"  data-toggle="modal" data-target="#myModal<?php echo $b_id; ?>">
 						<img class="img-rounded" src="<?php echo $bImage;?>" height="350" width="350">
 		  			</a>
+		  			<div class="row" style="margin-top: -20px">
+		  				<strong><?php echo $title ?></strong>
+		  			</div>
 			 	</div>
 			 </center>
 		<?php endforeach; ?>
